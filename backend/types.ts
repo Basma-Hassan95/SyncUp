60ab3654b6deeca7b61395b1db20743d8c3303bc
Basma-Hassan95 <basmakhan220@gmail.com>
2026-02-12 17:52:49 +0500

    first-commit

---
+++ backend/types.ts
@@ -0,0 +1,21 @@
+import type { Document, Types } from "mongoose";
+
+export interface UserProps  {
+  email: string;
+  password: string;
+  name?: string;
+  avatar?: string;
+  created?: Date;
+}
+
+export interface ConversationProps  {
+  
+  type: "direct" | "group";
+  name?: string;
+  participants: Types.ObjectId[];
+  lastMessage?: Types.ObjectId;
+  createdBy?: Types.ObjectId;
+  avatar?: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
---
+++ components/Input.tsx
@@ -0,0 +1,64 @@
+import { StyleSheet, Text, View, TextInput, } from 'react-native'
+import React from 'react'
+import { InputProps } from '@/types'
+import { useIsFocused } from '@react-navigation/native'
+import { useState } from 'react'
+import { colors, spacingX } from '@/constants/theme'
+import { verticalScale } from '@/utils/styling'
+import { radius } from '@/constants/theme'
+
+const Input = (props: InputProps) => {
+
+    const [isFocused, setIsFocused] = useState(false);
+  return (
+    <View
+    style={[
+        styles.container,
+        props.containerStyle && props.containerStyle,
+        isFocused && styles.primaryBorder
+    ]}
+    >
+      {props.icon && props.icon}
+
+      <TextInput
+      style={[styles.input, props.inputStyle]}
+      placeholderTextColor={colors.neutral400}
+      ref={props.inputRef && props.inputRef}
+      onFocus={() => setIsFocused(true)}
+      onBlur={() => setIsFocused(true)}
+      {...props}
+
+      />
+    </View>
+  )
+}
+
+export default Input
+
+const styles = StyleSheet.create({
+    container:{
+        flexDirection: 'row',
+        height: verticalScale(56),
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderWidth: 1,
+        borderColor: colors.neutral200,
+        borderRadius: radius.full,
+        borderCurve: 'continuous',
+        paddingHorizontal: spacingX._15,
+        backgroundColor: colors.neutral100,
+        gap: spacingX._10,
+
+
+    },
+    primaryBorder:{
+        borderColor: colors.primary,
+
+    },
+    input:{
+     flex:1,
+     color: colors.text,
+     fontSize: verticalScale(14),
+
+    }
+})
